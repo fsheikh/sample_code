b57698e1735706bac722f134de28e52b3f3eb816
Faheem Sheikh <fahim.sheikh@gmail.com>
2020-01-26 20:54:56 +0100

    mfcc experiments
    Ran mfcc experiments for a ghazal and two qawalis. Added logging and png saving support for resulting librosa plots.

--- musik/audio_features_extraction.py
+++ musik/audio_features_extraction.py
@@ -12,74 +12,83 @@ import librosa.display as disp
 import gdown as gd
 import matplotlib.pyplot as plt
 import os
+import logging
 
-# TODO: Conver into lists to process more than one songs
-song_url = 'https://drive.google.com/uc?id=1oB3wFoZgHGE5gnP88KV505AcFrnEHfz_'
-song_location = 'C:\\personal\\musik\q1.mp3'
 
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
+
+
+# Links from eigene data-set
+piya_say_naina = 'https://drive.google.com/uc?id=1oB3wFoZgHGE5gnP88KV505AcFrnEHfz_'
+khawaja = 'https://drive.google.com/uc?id=165I1Uf8LqaAiBm750NOxLbeDy8isJf3R'
+yt_rumi_qawali='https://www.youtube.com/watch?v=FjggJH45RRE'
+zikar_parivash_ka = 'https://drive.google.com/uc?id=1L5VRFhhCF3oaQWyfynip_z_jVUQpqp07'
+
+local_directory = 'C:\\personal\\musik'
+url_local_map = {piya_say_naina : local_directory+'\\q1.mp3',
+				 khawaja : local_directory+'\\q2.mp3',
+				 zikar_parivash_ka : local_directory+'\\g1.mp3'}
+
+logger.info(url_local_map)
 
 # if same file already has been downloaded skip it
 
-if os.path.exists(song_location):
-	print('File already exists, skipping Download')
+if os.path.exists(url_local_map[khawaja]):
+	logger.info('File %s already exists, skipping Download', url_local_map[khawaja])
 else: 
-	gd.download(song_url, song_location, quiet=False, proxy=None)
+	gd.download(khawaja, url_local_map[khawaja], quiet=False, proxy=None)
 
-
+aSR = 22050
+observeDuration = 120
 # Taken from Librosa tutorial
-y, sr = rosa.load(song_location)
+y, sr = rosa.load(url_local_map[khawaja], sr=aSR, mono=True, offset=0.0, duration=observeDuration)
 
-# Set the hop length; at 22050 Hz, 1024 samples ~= 46
+if (sr != aSR): 
+	logger.warning("Determined sample rate=%d different from assumed!", sr)
+
+# Set the hop length; at 22050 Hz, 1024 samples ~= 46ms
 hop_length = 1024
 
 # Separate harmonics and percussives into two waveforms
 y_harmonic, y_percussive = rosa.effects.hpss(y)
 
-print('Audio file loaded, harmonics, percussion separated...')
+logger.info('Audio file loaded, harmonics, percussion separated...')
 
 # Beat track on the percussive signal
 tempo, beat_frames = rosa.beat.beat_track(y=y_percussive,
                                              sr=sr)
 
-print('Computing MFCCs')
+logger.info('Computing MFCCs')
 
 # Compute MFCC features from the raw signal
-mfcc = rosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+mfcc = rosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=5)
 
-plt.subplot(2,2,1)
+ax = plt.subplot(2,2,1)
 disp.specshow(mfcc, x_axis='time', y_axis='mel', sr=sr, hop_length=hop_length)
+
+ax.title.set_text('mfcc')
 # And the first-order differences (delta features)
 mfcc_delta = rosa.feature.delta(mfcc)
-
-plt.subplot(2,2,2)
+ax = plt.subplot(2,2,2)
 disp.specshow(mfcc_delta, x_axis='time', y_axis='mel', sr=sr, hop_length=hop_length)
-
+ax.title.set_text('delta-mfcc')
 # Stack and synchronize between beat events
 # This time, we'll use the mean value (default) instead of median
 beat_mfcc_delta = rosa.util.sync(np.vstack([mfcc, mfcc_delta]),beat_frames)
+ax=plt.subplot(2,2,3)
+disp.specshow(beat_mfcc_delta, x_axis='time', sr=sr, hop_length=hop_length)
+ax.title.set_text('beat-mfcc')
 
-#disp.specshow(beat_mfcc_delta, x_axis='time', sr=sr, hop_length=hop_length)
 
-
-print('Computing chromagram')
+logger.info('Computing chromagram')
 # Compute chroma features from the harmonic signal
 chromagram = rosa.feature.chroma_cqt(y=y_harmonic,
                                         sr=sr)
 
-plt.subplot(2,2,3)
+ax = plt.subplot(2,2,4)
 disp.specshow(chromagram, cmap='gray_r', y_axis='chroma', sr=sr, hop_length=hop_length)
-
-# Aggregate chroma features between beat events
-# We'll use the median value of each feature between beat frames
-beat_chroma = rosa.util.sync(chromagram,
-                                beat_frames,
-                                aggregate=np.median)
-
-# Finally, stack all beat-synchronous features together
-#beat_features = np.vstack([beat_chroma, beat_mfcc_delta])
-
-plt.subplot(2,2,4)
-disp.specshow(beat_mfcc_delta, x_axis='time', sr=sr, hop_length=hop_length)
+ax.title.set_text('chroma')
 
 plt.show()
-print('Done with feature extraction!')
+logger.info('Done with feature extraction!')
