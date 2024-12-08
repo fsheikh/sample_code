be6a6dd65a0c5f08a0a8b4e0299dbc32720c1696
Muhammad Taha <tahafaisal9292@gmail.com>
2024-12-08 19:46:31 +0500

    Server Side chnages migrated to SQLite

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import sqlite3 from 'sqlite3'; // Import sqlite3
+import blogControllerRoutes from './Controllers/blogControllers.js';
+
+const app = express();
+const PORT = 3030;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Initialize SQLite3 database
+const db = new sqlite3.Database('deurdu.db', (err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the SQLite database');
+  }
+});
+
+// Make the database connection available to routes
+app.use((req, res, next) => {
+  req.db = db; // Attach the database connection to the request object
+  next();
+});
+
+// Routes
+app.use('/blogs', blogControllerRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
