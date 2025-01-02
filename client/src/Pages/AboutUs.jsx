0ddca3706fd7ba8b6f03f3f46207508b7c8f62c9
Muhammad Taha <tahafaisal9292@gmail.com>
2025-01-03 00:06:34 +0500

    Link routes to the pages were added + those pages were created too

---
+++ client/src/Pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <Navbar/>
+        AboutUs
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ client/src/Pages/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const ContactUs = () => {
+  return (
+    <div>
+        <Navbar/>
+        ContactUs
+    </div>
+  )
+}
+
+export default ContactUs
