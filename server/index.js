06ea862f32ed447f5778e0eba4015a7523432b9b
Muhammad Taha <tahafaisal9292@gmail.com>
2025-01-02 22:38:08 +0500

    authController files was deleted and commments were added to the blog controller file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import sqlite3 from 'sqlite3'; // Import sqlite3
 import blogControllerRoutes from './Controllers/blogControllers.js';
+import { DB_NAME } from './Config.js' // importing the database name from the config.js file
 
 const app = express();
 const PORT = 3030;
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Initialize SQLite3 database
-const db = new sqlite3.Database('deurdu.db', (err) => {
+const db = new sqlite3.Database(DB_NAME, (err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
   } else {
